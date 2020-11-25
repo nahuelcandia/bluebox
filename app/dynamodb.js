256db72ba4e63fb6a3c9f784124bd61f9f72cf21
Nahuel Candia <Nahuel Candia>
2020-11-25 17:34:22 -0300

    add dynamoose, define model, getfromDb

--- app/dynamodb.js
+++ app/dynamodb.js
@@ -1,27 +1,38 @@
-'use strict';
+//https://aws.amazon.com/blogs/database/applying-best-practices-for-securing-sensitive-data-in-amazon-dynamodb/
 
-const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const dynamoose = require("dynamoose");
 
-let options = {};
+const ddb = new dynamoose.aws.sdk.DynamoDB({
+  "accessKeyId": process.env.DB_ACCESSKEYID,
+  "secretAccessKey": process.env.DB_SECRETACCESSKEY,
+  "region": process.env.DB_REGION
+});
 
-// connect to local DB if running offline
-if (process.env.IS_OFFLINE) {
-  options = {
-    region: 'localhost',
-    endpoint: 'http://localhost:3000',
-  };
+if(process.env.NODE_ENV === 'dev') {
+  dynamoose.aws.ddb.local();
+} else {
+  dynamoose.aws.ddb.set(ddb);
 }
 
-const client = new AWS.DynamoDB.DocumentClient(options);
+const blueboxData = dynamoose.model("bluebox-"+process.env.NODE_ENV, new dynamoose.Schema({
+    "alias": {
+      "type": String,
+      "hashKey": true
+    }, 
+    "value": String, 
+    "ttl": Number
+  }), { 
+    "saveUnknown": false,
+    "timestamps": true,
+    "create": false
+});
 
-module.exports = client;
 
-module.exports.saveInDb = async function(rawData) {
+module.exports.saveInDb = async function(rawData, expiresExpire) {
   let alias = rawData;
   return alias
 }
 
 module.exports.getFromDb = async function(alias) {
-  let encodedValue = alias;
-  return encodedValue
+  return await blueboxData.query("alias").eq(alias).limit(1)
 }
