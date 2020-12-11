abb14e77d16316a8fb9d15cab2c6cb7fb23b20ac
Nahuel Candia <Nahuel Candia>
2020-12-11 20:01:23 -0300

    support regex within values to encrypt

--- app/bluebox.js
+++ app/bluebox.js
@@ -3,7 +3,7 @@ const replacementRules = require("../config/replacementRules.json");
 const { encodeSensibleData, decodeSensibleData } = require("./cypher");
 const { saveInDb, getFromDb } = require("./dynamodb");
 
-exports.blueboxReplacer = async function (cypherAction, data) {
+exports.blueboxReplacer = async function (cypherAction, request) {
     // Searches for parameters to replace in the data
     // based on the config/replacementRules.json
     // for each item found it will encrypt or decrypt the data.
@@ -13,7 +13,8 @@ exports.blueboxReplacer = async function (cypherAction, data) {
             let responseData = {}
             for(var i= 0; i < replacementRules.length; i++) {
                 let ttl = (replacementRules[i].ttl)? replacementRules[i].ttl : null;
-                responseData = await replaceAndCypher(data, replacementRules[i].attributeName, cypherAction, ttl);
+                let regex = (replacementRules[i].regex)? replacementRules[i].regex : null;
+                responseData = await replaceAndCypher(request, replacementRules[i].attributeName, cypherAction, ttl, regex);
             }
             resolve(responseData)
         } catch(e) {
@@ -22,30 +23,37 @@ exports.blueboxReplacer = async function (cypherAction, data) {
     });
 }
 
-async function replaceAndCypher(object, indexName, cypherAction, ttl){
+async function replaceAndCypher(request, ruleAttributeName, cypherAction, ttl, regex){
     return new Promise(async function(resolve, reject) {
         try {
-            for(var x in object){
-                if(typeof object[x] == typeof {}){
-                  await replaceAndCypher(object[x], indexName, cypherAction, ttl);
+            for(var property in request){
+                if(typeof request[property] == typeof {}){
+                  await replaceAndCypher(request[property], ruleAttributeName, cypherAction, ttl, regex);
                 }
-                if(x === indexName){ 
-                  object[indexName] = await cypher(cypherAction, object[indexName], ttl);
+                if(property === ruleAttributeName){ 
+                    request[property] = await cypher(cypherAction, request[property], ttl, regex);
                 }
             }
-            resolve(object); 
+            resolve(request); 
         } catch (e) {
             reject(e);
         }
     });
 }
 
-async function cypher(action, value, ttl) {
+async function cypher(action, value, ttl, regex) {
+    regex = (regex)? new RegExp(regex) : null;
     if(action) {
-        let encodedValue = await getFromDb(value); //the value is the alias
-        return await decodeSensibleData(encodedValue); //return raw data
+        let alias = (regex)? value.match(regex) : value;
+        let encodedValue = await getFromDb(alias); //the value is the alias
+        let rawData = await decodeSensibleData(encodedValue);
+        rawData = (regex)? value.replace(regex, rawData) : rawData;
+        return rawData
     } else {
-        let encodedValue = await encodeSensibleData(value); //value is the raw data
-        return await saveInDb(encodedValue, ttl) //return alias
+        let valueToEncrypt = (regex)? value.match(regex) : value;
+        let encodedValue = await encodeSensibleData(valueToEncrypt); //value is the raw data
+        let alias = await saveInDb(encodedValue, ttl); //return alias
+        alias = (regex)? value.replace(regex, alias) : alias; //if has a regex returns the request as is, replacing the sensitive part.
+        return alias
     }
 }
