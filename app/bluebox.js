8288444379ee8f8ba533e921a453e68ff5720422
Nahuel Candia <Nahuel Candia>
2020-12-15 17:15:17 -0300

    default behaviour for CVV and encrypt/decrypt partial PAN

--- app/bluebox.js
+++ app/bluebox.js
@@ -13,8 +13,8 @@ exports.blueboxReplacer = async function (cypherAction, request) {
             let responseData = {}
             for(var i= 0; i < replacementRules.length; i++) {
                 let ttl = (replacementRules[i].ttl)? replacementRules[i].ttl : null;
-                let regex = (replacementRules[i].regex)? replacementRules[i].regex : null;
-                responseData = await replaceAndCypher(request, replacementRules[i].attributeName, cypherAction, ttl, regex);
+                let type = (replacementRules[i].type)? replacementRules[i].type : null;
+                responseData = await replaceAndCypher(request, replacementRules[i].attributeName, cypherAction, ttl, type);
             }
             resolve(responseData)
         } catch(e) {
@@ -23,15 +23,15 @@ exports.blueboxReplacer = async function (cypherAction, request) {
     });
 }
 
-async function replaceAndCypher(request, ruleAttributeName, cypherAction, ttl, regex){
+async function replaceAndCypher(request, ruleAttributeName, cypherAction, ttl, type){
     return new Promise(async function(resolve, reject) {
         try {
             for(var property in request){
                 if(typeof request[property] == typeof {}){
-                  await replaceAndCypher(request[property], ruleAttributeName, cypherAction, ttl, regex);
+                  await replaceAndCypher(request[property], ruleAttributeName, cypherAction, ttl, type);
                 }
                 if(property === ruleAttributeName){ 
-                    request[property] = await cypher(cypherAction, request[property], ttl, regex);
+                    request[property] = await cypher(cypherAction, request[property], ttl, type);
                 }
             }
             resolve(request); 
@@ -41,19 +41,42 @@ async function replaceAndCypher(request, ruleAttributeName, cypherAction, ttl, r
     });
 }
 
-async function cypher(action, value, ttl, regex) {
-    regex = (regex)? new RegExp(regex) : null;
-    if(action) {
-        let alias = (regex)? value.match(regex) : value;
-        let encodedValue = await getFromDb(alias); //the value is the alias
-        let rawData = await decodeSensibleData(encodedValue);
-        rawData = (regex)? value.replace(regex, rawData) : rawData;
-        return rawData
-    } else {
-        let valueToEncrypt = (regex)? value.match(regex) : value;
-        let encodedValue = await encodeSensibleData(valueToEncrypt); //value is the raw data
-        let alias = await saveInDb(encodedValue, ttl); //return alias
-        alias = (regex)? value.replace(regex, alias) : alias; //if has a regex returns the request as is, replacing the sensitive part.
-        return alias
+async function cypher(action, value, ttl, type) {
+    try {
+        if(action) {
+            let alias = value.match(/bx_\w+_bx/g)[0];
+            let encodedValue = await getFromDb(alias); //the value is the alias
+            let rawData = await decodeSensibleData(encodedValue);
+            rawData = (type === 'PAN')? value.replace(alias, rawData) : rawData;
+            return rawData
+        } else {
+            value = (type === 'PAN')? value.replace(/\D/g, '') : value; //remove spaces and - if PAN
+            let valueToEncrypt = (type === 'PAN')? await getMiddleDigits(value) : value;
+            console.log(valueToEncrypt)
+            ttl = (type === 'CVV' && !ttl)? 3600 : ttl; //If the value to encode is a CVV type, but TTL is not defined it will save it with 3h expiration by default.
+            let encodedValue = await encodeSensibleData(valueToEncrypt); //value is the raw data
+            let alias = await saveInDb(encodedValue, ttl); //return alias
+            console.log(alias)
+            alias = (type === 'PAN')? value.replace(valueToEncrypt, alias) : alias; //if has a type returns the request as is, replacing the sensitive part.
+            console.log('--->'+alias)
+            return alias
+        }
+    } catch (error) {
+        console.log(error)
+        throw error
     }
+}
+
+const extractFirstPart = (code) => {
+	const chunk = code.match(/\w+bx_/g)[0]
+	return chunk.substring(0, chunk.length-3)
+}
+const extractLastPart = (code) => {
+	const chunk = code.match(/\_bx\w+/g)[0]
+	return chunk.substr(3)
+}
+
+async function getMiddleDigits(cardNumber) {
+    //Gets everything but the first 6 and last 4 digits.
+    return cardNumber.substring(6, cardNumber.length-4);
 }
